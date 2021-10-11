6c66e8a5b7cb27511143b07426d10089d355d8e8
Chiayiau <90165973+Chiayiau@users.noreply.github.com>
2021-10-11 10:45:53 +0800

    Add files via upload

---
+++ lab 5.1.py
@@ -0,0 +1,12 @@
+# Student ID : 1201200302 #
+# Student Name : Liang Chia Yiau 
+
+def my_fuc(name, contact_no, salary, overtime):
+    print("Hello my name is : ",name)
+    print("This is my phone number ",contact_no)
+    total=float(salary) + float(overtime)
+    print("Your salary this month is ", total)
+
+my_fuc("Chiayiau","0182525581", 4000, 500)
+
+
---
+++ lab 5.2.py
@@ -0,0 +1,12 @@
+# Student ID : 1201200302 #
+# Student Name : Liang Chia Yiau 
+
+fruits = ["apple","banana","cherry"]
+
+def display_array(food):
+    for f in food:
+        print(f)
+
+
+display_array(fruits)
+
